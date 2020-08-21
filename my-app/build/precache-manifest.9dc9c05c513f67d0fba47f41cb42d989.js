6caf838d812d5f4bbee9a77e9e6c45b1696dd8ff
Mariya <alisa-m2@yandex.ru>
2020-08-21 21:01:37 +0300

    New app

---
+++ my-app/build/precache-manifest.9dc9c05c513f67d0fba47f41cb42d989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29178028441675d0f2220aadebca77c6",
+    "url": "/alisam1/3DaVinci_test/static/media/heart_logo.29178028.svg"
+  },
+  {
+    "revision": "887c5e37b4ea918ce35e",
+    "url": "/alisam1/3DaVinci_test/static/js/runtime~main.887c5e37.js"
+  },
+  {
+    "revision": "b874a49f4133c0ed25cd",
+    "url": "/alisam1/3DaVinci_test/static/js/main.b874a49f.chunk.js"
+  },
+  {
+    "revision": "22ca81e4b45925ecc5f3",
+    "url": "/alisam1/3DaVinci_test/static/js/1.22ca81e4.chunk.js"
+  },
+  {
+    "revision": "b874a49f4133c0ed25cd",
+    "url": "/alisam1/3DaVinci_test/static/css/main.192e1954.chunk.css"
+  },
+  {
+    "revision": "afed7edbf020843b451ecc78b43a7095",
+    "url": "/alisam1/3DaVinci_test/index.html"
+  }
+];
