f0089c84d78c1b652058589b5e23873486383065
Mariya <alisa-m2@yandex.ru>
2020-08-21 13:41:49 +0300

    New folder my-app

---
+++ my-app/src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './sass/style.scss'
+
+class Header extends Component {
+
+    render(){
+      return (
+        <header className="header">
+          <div className="header__content">
+            <h1 className="header__title">Lorem ipsum dolor sit amet</h1>
+          </div>
+        </header>
+      )
+    }
+}
+
+export default Header;
