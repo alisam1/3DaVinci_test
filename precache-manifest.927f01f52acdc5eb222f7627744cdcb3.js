dc8e31512c75eac605a052e12ddf79a30cfcbc9d
Mariya <alisa-m2@yandex.ru>
2020-08-21 21:52:00 +0300

    Updates

---
+++ precache-manifest.927f01f52acdc5eb222f7627744cdcb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29178028441675d0f2220aadebca77c6",
+    "url": "/3DaVinci_test/static/media/heart_logo.29178028.svg"
+  },
+  {
+    "revision": "1ab7e6ea842e8795aedb",
+    "url": "/3DaVinci_test/static/js/runtime~main.1ab7e6ea.js"
+  },
+  {
+    "revision": "c4c4a01771d038ab0c16",
+    "url": "/3DaVinci_test/static/js/main.c4c4a017.chunk.js"
+  },
+  {
+    "revision": "22ca81e4b45925ecc5f3",
+    "url": "/3DaVinci_test/static/js/1.22ca81e4.chunk.js"
+  },
+  {
+    "revision": "c4c4a01771d038ab0c16",
+    "url": "/3DaVinci_test/static/css/main.4df6cc4a.chunk.css"
+  },
+  {
+    "revision": "2927616c5ca7efb06ba7116e5abe17cd",
+    "url": "/3DaVinci_test/index.html"
+  }
+];
